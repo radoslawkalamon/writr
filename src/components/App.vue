c840f90b3eae8148a3f84033867990d0700bcf20
Radosław Kuświk <radoslaw.kuswik@vml.com>
2018-08-14 17:04:24 +0200

    [α] Initial commit

---
+++ src/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>{{this.$store.state.count}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'writr-vue2',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" src="../style/style.scss" />
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+import state from './state.js';
+import getters from './getters.js';
+import actions from './actions.js';
+import mutations from './mutations.js';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
+});
