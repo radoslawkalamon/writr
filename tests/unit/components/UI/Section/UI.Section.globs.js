9a01c494810c072f99c83e32499f6605f4fd84db
radoslawkuswik <radoslaw.kuswik@gmail.com>
2018-12-12 12:52:12 +0100

    [β] UI.Section unit tests done

--- tests/unit/components/UI/Section/UI.Section.globs.js
+++ tests/unit/components/UI/Section/UI.Section.globs.js
@@ -1,11 +1,9 @@
 const TEST_NAME = 'test-name';
 const TEST_TITLE = 'This is test title!';
-const TEST_TEXT = 'This is test text!';
 
 export default {
   test: {
     name: TEST_NAME,
     title: TEST_TITLE,
-    text: TEST_TEXT,
   },
 };
