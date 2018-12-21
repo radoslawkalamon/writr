9f88ace46ab4a6e1896a55b88ac4f1c5c896cda3
Radosław Kuświk <radoslaw.kuswik@gmail.com>
2018-12-21 14:04:08 +0100

    [β] Statusbar/Item unit tests done

---
+++ tests/unit/components/Statusbar/Item/Statusbar.Item.globs.js
@@ -0,0 +1,11 @@
+const TEST_VALUE = 'Test value';
+const TEST_LABEL = 'Test Label';
+const TEST_NAME = 'test-name';
+
+export default {
+  test: {
+    value: TEST_VALUE,
+    label: TEST_LABEL,
+    name: TEST_NAME,
+  },
+};
